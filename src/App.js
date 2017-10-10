2b9591eb73d148fb7bd4b79847a184b9b642d9f1
haidy <haidy>
2017-10-10 09:50:24 -0500

    Sing Up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Home from './Home';
 import Datos from './Datos'; 
 
 import {
@@ -10,35 +11,6 @@ import {
     Switch
   } from 'react-router-dom';
 
-const Home = () => {
-  return(<div className="index">
-   <section className="bienvenida">
-      <div className="container-fluid">
-          <div className="container">
-              <div className="row">
-                  <div className="col-md-12 col-sm-12 col-xs-12 text-center">
-                      <img src="http://www.whatsthefare.com/static/img/lyft_logo.png" alt="" width="150"/>
-                  </div>
-              </div>
-          </div>
-           <div className="container btn-login">
-               <div className="row">
-                   <div className="col-md-6 col-sm-6 col-xs-6 text-center">
-                       <a to="/about" className="btn btn-default btn-lg active login" role="button">Log in</a>
-                   </div>
-                   <div className="col-md-6 col-sm-6 col-xs-6 text-center">
-                   <ul>
-                   <li className="btn btn-default btn-lg active init"><Link className="btn btn-default btn-lg active init" to="/datos">Sing Up</Link></li>
-                    </ul>
-               </div>
-               </div>
-           </div>
-       </div>
-   </section>
-</div>);
-}
-
-
 class App extends Component {
     render() {
       return (
@@ -46,6 +18,7 @@ class App extends Component {
         <div className="container">
             <Switch>
               <Route exact path="/" component={Home} />
+              <Route path="/home" component={Home} />
               <Route path="/datos" component={Datos} />
               <Route render={() => <Redirect to="/" />} />
             </Switch>
--- src/Datos.js
+++ src/Datos.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
+import Home from './Home'; 
 import './App.css';
+import {
+    BrowserRouter,
+    Route,
+    Link,
+    Redirect,
+    Switch
+  } from 'react-router-dom';
 
 class Datos extends Component {
 	render() {
 		return (
+            <div>
+                <div className="container-fluid">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-2 col-sm-2 col-xs-2">
+                                <div className="back-page1 text-center">
+                                <ul>
+                                    <li className="btn btn-default btn-lg active init"><Link className="btn btn-default btn-lg active init" to="/home">Sing Up</Link ></li>
+                                </ul>
+                                </div>
+                            </div>
+                            <div className="col-md-8 col-sm-8 col-xs-8">
+                                <div className="title-signup text-center">
+                                    <h1>Sign up</h1>
+                                    <p>Join now for free ride credit</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             <form data-toggle="validator">
                 <div className="form-group">
                     <label htmlFor="inputName" className="control-label">Name</label>
@@ -27,6 +55,7 @@ class Datos extends Component {
                     <button type="submit" className="btn btn-primary">Submit</button>
                 </div>
             </form>
+            </div>
 		);
 	}
 }
