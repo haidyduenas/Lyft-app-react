fc7d53fe458878c6ce346c7b116eedec918ef8c6
haidy <haidy>
2017-10-10 15:00:57 -0500

    Commits

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import './App.css';
 
 import Home from './Home';
 import SignUp from './SignUp';
+import Code from './Code';
 import SignUpForm from './SignUpForm';
 import LyftMap from './LyftMap';
 
@@ -29,6 +30,7 @@ const App = (props) => {
 				       render={() => <Redirect to= {'/home'}/>}/>
 				<Route  path="/home" render={() => <Home model={model} />}/>
 				<Route  path="/signup" render={() => <SignUp model={model} />}/>
+				<Route  path="/code" render={() => <Code model={model} />}/>
 				<Route  path="/signup-form" render={() => <SignUpForm model={model} />}/>
 				<Route  path="/lyftmap" render={() => <LyftMap model={model} />}/>
 				<Route component={NotFound}/>
